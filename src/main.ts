337ee0938dd5c7e56d2125206147bd5acc61894d
Jukka Aittokallio <jai@reaktor.fi>
2020-04-03 17:57:26 +0300

    Add command line parsing

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,54 @@
-import processLocales from "./index";
+import processLocales, { Opts } from "./index";
 
-const args = process.argv.slice(2);
-
-if (args.length < 2) {
-  console.log("usage: node find <src dir> <dest dir>");
-  process.exit(1);
+function makeRegexMatcher(re: string) {
+  const regexp = new RegExp(re);
+  return (path: string) => regexp.test(path);
 }
 
-processLocales({ srcDir: args[0], destDir: args[1] })
-  .then(res => console.log(JSON.stringify(res, null, 2)))
-  .catch(err => console.error(err.message));
+function parseArguments() {
+  const opts: Partial<Opts> = {};
+  const args = process.argv.slice(2);
+
+  function checkOpt(v: string | undefined, msg: string): string {
+    if (!v) {
+      console.error(msg);
+      process.exit(1);
+    }
+    return v;
+  }
+
+  let i = 0;
+  const params = [] as string[];
+  while (i < args.length) {
+    switch (args[i]) {
+      case "-j":
+        opts.ts = false;
+        break;
+      case "-p":
+        opts.prefix = checkOpt(args[++i], "-p option needs a prefix string");
+        break;
+      case "-r":
+        opts.isLocaleFile = makeRegexMatcher(
+          checkOpt(args[++i], "-r option needs a regular expression.")
+        );
+        break;
+      default:
+        params.push(args[i]);
+        break;
+    }
+    i++;
+  }
+
+  if (params.length < 2) {
+    console.log(
+      "usage: node find [-j] [-p <prefix>] [-r regex] <src dir> <dest dir>"
+    );
+    process.exit(1);
+  }
+
+  opts.srcDir = params[0];
+  opts.destDir = params[1];
+  return opts;
+}
+
+processLocales(parseArguments()).catch(err => console.error(err.message));
